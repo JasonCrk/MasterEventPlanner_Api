dc33454d14b586699a755cec12db292ebfd2b25c
JasonCrk <emerzonkolki@gmail.com>
2024-04-02 00:11:24 -0500

    feat: Initial commit

---
+++ src/main/java/com/EventPlanCorp/MasterEventPlanner/MasterEventPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.EventPlanCorp.MasterEventPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterEventPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterEventPlannerApplication.class, args);
+	}
+
+}
