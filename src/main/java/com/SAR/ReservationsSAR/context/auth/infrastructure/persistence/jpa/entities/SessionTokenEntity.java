77f17b985cd611e3cb8d07ade219a4329f3a7c03
JasonCrk <emerzonkolki@gmail.com>
2024-04-17 23:01:57 -0500

    feat(SessionTokenEntity.java): Change session token table

--- src/main/java/com/SAR/ReservationsSAR/context/auth/infrastructure/persistence/jpa/entities/SessionTokenEntity.java
+++ src/main/java/com/SAR/ReservationsSAR/context/auth/infrastructure/persistence/jpa/entities/SessionTokenEntity.java
@@ -14,7 +14,7 @@ import lombok.NoArgsConstructor;
 import java.util.UUID;
 
 @Entity
-@Table(name = "session_tokens")
+@Table(name = "session_token")
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
