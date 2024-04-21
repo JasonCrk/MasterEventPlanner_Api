7c314d77fd6df6625846c1a51939438448ff7d58
JasonCrk <emerzonkolki@gmail.com>
2024-04-20 23:38:58 -0500

    feat: Topic list response mapping

--- src/main/java/com/SAR/ReservationsSAR/context/establishment/application/services/EstablishmentService.java
+++ src/main/java/com/SAR/ReservationsSAR/context/establishment/application/services/EstablishmentService.java
@@ -3,7 +3,7 @@ package com.SAR.ReservationsSAR.context.establishment.application.services;
 import com.SAR.ReservationsSAR.context.establishment.domain.responses.EstablishmentDetailsResponse;
 import com.SAR.ReservationsSAR.context.establishment.domain.responses.EstablishmentItemResponse;
 import com.SAR.ReservationsSAR.context.establishment.domain.Establishment;
-import com.SAR.ReservationsSAR.context.topic.domain.Topic;
+import com.SAR.ReservationsSAR.context.topic.domain.responses.TopicResponse;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -12,7 +12,7 @@ import java.util.UUID;
 public interface EstablishmentService {
     EstablishmentDetailsResponse getEstablishmentDetails(UUID establishmentId);
 
-    List<Topic> getEstablishmentTopics(UUID establishmentId);
+    List<TopicResponse> getEstablishmentTopics(UUID establishmentId);
 
     List<EstablishmentItemResponse> searchAvailableEstablishments(
             LocalDateTime realizationDate,
--- src/main/java/com/SAR/ReservationsSAR/context/topic/domain/mappers/TopicMapper.java
+++ src/main/java/com/SAR/ReservationsSAR/context/topic/domain/mappers/TopicMapper.java
@@ -2,8 +2,11 @@ package com.SAR.ReservationsSAR.context.topic.domain.mappers;
 
 import com.SAR.ReservationsSAR.context.topic.domain.responses.TopicResponse;
 import com.SAR.ReservationsSAR.context.topic.domain.Topic;
+
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface TopicMapper {
@@ -11,4 +14,6 @@ public interface TopicMapper {
     TopicMapper INSTANCE = Mappers.getMapper(TopicMapper.class);
 
     TopicResponse toResponse(Topic topic);
+
+    List<TopicResponse> toListResponse(List<Topic> topics);
 }
