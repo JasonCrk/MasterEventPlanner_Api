fdd87b49efd6aa88f212af3d33a8c44dd1ea1876
JasonCrk <emerzonkolki@gmail.com>
2024-04-28 23:36:40 -0500

    fix(context/auth/application/controller): Validate requests

--- src/main/java/com/SAR/ReservationsSAR/context/auth/application/controller/AuthController.java
+++ src/main/java/com/SAR/ReservationsSAR/context/auth/application/controller/AuthController.java
@@ -10,14 +10,15 @@ import com.SAR.ReservationsSAR.shared.domain.responses.MessageResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 
 public interface AuthController {
     ResponseEntity<UserMe> getMe(@RequestAttribute("user") User authUser);
-    ResponseEntity<JwtResponse> login(@RequestBody LoginRequest request);
-    ResponseEntity<JwtResponse> registerUser(@RequestBody RegisterUserRequest request);
+    ResponseEntity<JwtResponse> login(@Valid @RequestBody LoginRequest request);
+    ResponseEntity<JwtResponse> registerUser(@Valid @RequestBody RegisterUserRequest request);
     ResponseEntity<MessageResponse> verifyToken(HttpServletRequest request);
     ResponseEntity<JwtResponse> refreshToken(HttpServletRequest request, HttpServletResponse response);
 }
--- src/main/java/com/SAR/ReservationsSAR/context/auth/application/controller/AuthControllerImpl.java
+++ src/main/java/com/SAR/ReservationsSAR/context/auth/application/controller/AuthControllerImpl.java
@@ -16,9 +16,11 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Auth")
+@Validated
 @RestController
 @RequestMapping("/api/v1/auth")
 public class AuthControllerImpl implements AuthController {
